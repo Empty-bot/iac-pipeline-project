83549dc7e8665d73c185389d2bcc2a867ec6bd63
fayelise <faye.elise@uam.edu.sn>
2025-12-24 19:55:11 +0000

    fIXED PIPELINE

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,15 @@
-resource "null_resource" "vm" {
+resource "null_resource" "vagrant_vm" {
+  # Trigger rebuild si Vagrantfile change
+  triggers = {
+    vagrantfile_hash = filesha256("../vagrant/Vagrantfile")
+  }
 
+  # Provision VM
   provisioner "local-exec" {
     command = "cd ../vagrant && vagrant up"
   }
 
+  # Cleanup VM
   provisioner "local-exec" {
     when    = destroy
     command = "cd ../vagrant && vagrant destroy -f"
