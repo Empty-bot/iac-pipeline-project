b1f856ce3ba21b70251e4c1b14f05a770e2264e8
fayelise <faye.elise@uam.edu.sn>
2025-12-26 16:31:18 +0000

    deadly working

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,19 +1,25 @@
-resource "null_resource" "vagrant_vm" {
-  triggers = {
-    vagrantfile_hash = filesha256("../vagrant/Vagrantfile")
-  }
+#resource "null_resource" "vagrant_vm" {
+#  triggers = {
+ #   vagrantfile_hash = filesha256("../vagrant/Vagrantfile")
+ # }
 
   # Provision VM with Ansible
-  provisioner "local-exec" {
-    command = <<EOT
-cd ../vagrant && vagrant up
-ansible-playbook -i ../ansible/inventory/hosts.ini ../ansible/playbook.yml --limit vm
-EOT
-  }
+  #provisioner "local-exec" {
+   # command = <<EOT
+#cd ../vagrant && vagrant up
+#ansible-playbook -i ../ansible/inventory/hosts.ini ../ansible/playbook.yml --limit vm
+#EOT
+ # }
 
   # Cleanup VM
-  provisioner "local-exec" {
-    when    = destroy
-    command = "cd ../vagrant && vagrant destroy -f"
+  #provisioner "local-exec" {
+   # when    = destroy
+    #command = "cd ../vagrant && vagrant destroy -f"
+  #}
+#}
+# terraform/main.tf
+resource "null_resource" "placeholder" {
+  triggers = {
+    terraform_run = timestamp()
   }
 }
